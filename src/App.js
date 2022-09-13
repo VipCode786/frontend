ca2c3fe98cc01439fbc2d21b49596a89e11b699c
unknown <v07860@gmail.com>
2022-09-13 19:00:35 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import ContactUs from "./Pages/ContactUs";
+import Home from "./Pages/Home";
 
 
 
 function App() {
   return (
-    <div className="App">
-     Hii
+    <div>
+     <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/about" element={<About/>} />
+     <Route path="/contactus" element={<ContactUs />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
